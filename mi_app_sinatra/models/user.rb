9ae95cb708d7cb5f7e89ce6d2448c97c1a625bcd
moyanoagustin <moyanoagustin025@gmail.com>
2025-05-28 17:36:29 -0300

    comentarios en el modelo user

--- mi_app_sinatra/models/user.rb
+++ mi_app_sinatra/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :accounts
-  validates :first_name, presence: true
-  validates :dni, presence: true, uniqueness: true
+  has_many :accounts #tiene muchas cuentas
+  validates :first_name, presence: true #valida que el nombre este presente
+  validates :dni, presence: true, uniqueness: true #valida que el dni este presente y sea unico
 end
