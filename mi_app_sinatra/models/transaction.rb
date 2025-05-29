7f04d1f0b8de6270ff3aca033a4c915b40c79624
moyanoagustin <moyanoagustin025@gmail.com>
2025-05-29 16:08:25 -0300

    Añado comentarios a transaction y transfer

--- mi_app_sinatra/models/transaction.rb
+++ mi_app_sinatra/models/transaction.rb
@@ -1,10 +1,10 @@
 class Transaction < ActiveRecord::Base
   self.primary_key = 'transaction_id'
-  belongs_to :account, foreign_key: 'account_cvu', primary_key: 'cvu'
-  has_one :transfer, foreign_key: 'transaction_id', primary_key: 'transaction_id'
+  belongs_to :account, foreign_key: 'account_cvu', primary_key: 'cvu' #pertenece a una cuenta
+  has_one :transfer, foreign_key: 'transaction_id', primary_key: 'transaction_id' #tiene una transferencia
 
-  validates :amount, presence: true
-  validates :date, presence: true
-  validates :transaction_type, presence: true, inclusion: { in: %w[deposit withdrawal transfer] }
-  validates :account_cvu, presence: true
+  validates :amount, presence: true #valida que el monto este presente
+  validates :date, presence: true #vaida que la fecha este presente
+  validates :transaction_type, presence: true, inclusion: { in: %w[deposit withdrawal transfer] } #valida que el tipo de transaccion este presente y sea uno de los valores permitidos
+  validates :account_cvu, presence: true #valida que el cvu de la cuenta este presente
 end
