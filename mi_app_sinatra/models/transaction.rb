3f2ff2c4e4fb4f298b012eeaf5503ff3773cc34c
moyanoagustin <moyanoagustin025@gmail.com>
2025-05-29 16:27:03 -0300

    Agrego callback y validaciones a los modelos

--- mi_app_sinatra/models/transaction.rb
+++ mi_app_sinatra/models/transaction.rb
@@ -7,4 +7,35 @@ class Transaction < ActiveRecord::Base
   validates :date, presence: true #vaida que la fecha este presente
   validates :transaction_type, presence: true, inclusion: { in: %w[deposit withdrawal transfer] } #valida que el tipo de transaccion este presente y sea uno de los valores permitidos
   validates :account_cvu, presence: true #valida que el cvu de la cuenta este presente
+
+  validate :sufficient_balance, on: :create #valida que el balance sea suficiente para una transaccion de retiro o transferencia
+
+  after_create :apply_transaction #callback que se ejecuta despues de crear una transaccion
+
+  private
+
+  def sufficient_balance
+    if %w[withdrawal transfer].include?(transaction_type)
+      if account.balance < amount
+        errors.add(:base, "Saldo insuficiente para realizar la transacción") #error si el saldo es insuficiente
+      end
+    end
+  end
+
+  def apply_transaction
+    case transaction_type
+    when 'deposit'
+      account.increment!(:balance, amount)
+    when 'withdrawal'
+      account.decrement!(:balance, amount)
+    when 'transfer'
+      account.decrement!(:balance, amount) # Debitar de la cuenta origen
+      if transfer && transfer.to_account
+        transfer.to_account.increment!(:balance, amount) # Acreditar en la cuenta destino si existe la transferencia asociada
+      end
+    end
+  end
+
+
+
 end
--- mi_app_sinatra/models/transfer.rb
+++ mi_app_sinatra/models/transfer.rb
@@ -3,4 +3,23 @@ class Transfer < ActiveRecord::Base
   belongs_to :transaction, foreign_key: 'transaction_id', primary_key: 'transaction_id' #pertenece a una transaccion
   belongs_to :from_account, class_name: 'Account', foreign_key: 'from_cvu', primary_key: 'cvu' #pertenece a una cuenta de origen
   belongs_to :to_account, class_name: 'Account', foreign_key: 'to_cvu', primary_key: 'cvu' #pertenece a una cuenta de destino
+
+  validates :from_cvu, presence: true #valida que el cvu de origen este presente
+  validates :to_cvu, presence: true #valida que el cvu de destino este presente
+  validate :different_accounts #valida que las cuentas de origen y destino sean diferentes
+  validate :to_account_exists #valida que la cuenta de destino exista
+
+  private
+
+  def different_accounts
+    if from_cvu == to_cvu
+      errors.add(:to_cvu, "no puede ser igual a la cuenta de origen") #error si las cuentas son iguales
+    end
+  end
+
+  def to_account_exists
+    unless Account.exists?(cvu: to_cvu)
+      errors.add(:to_cvu, "debe corresponder a una cuenta existente") #error si la cuenta de destino no existe
+    end
+  end
 end
