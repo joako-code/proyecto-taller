244eb0b996e394f8d6434ed40937bb8a2116307a
Amade0o <amadeoemai@gmail.com>
2025-05-29 21:29:18 -0300

    - Inicializado RSpec: creado el directorio spec/ y el archivo de configuración .rspec

---
+++ mi_app_sinatra/spec/models/transaction_spec.rb
@@ -0,0 +1,44 @@
+require_relative '../spec_helper'
+require_relative '../../models/transaction'
+require_relative '../../models/account'
+require_relative '../../models/user'
+
+RSpec.describe Transaction do
+  let(:user) { User.create!(dni: '12345678', first_name: 'Test', last_name: 'User', phone: '123', email: 'test@example.com', password: '1234') }
+  let(:account) { Account.create!(cvu: 'CVU123', dni: user.dni, balance: 100) }
+
+  context 'validations' do
+    it 'no permite crear transacción si no hay saldo suficiente' do
+      transaction = Transaction.new(
+        account_cvu: account.cvu,
+        amount: 150,
+        date: Date.today,
+        transaction_type: 'withdrawal'
+      )
+      expect(transaction).not_to be_valid
+      expect(transaction.errors[:base]).to include("Saldo insuficiente para realizar la transacción")
+    end
+
+    it 'permite crear transacción si hay saldo suficiente' do
+      transaction = Transaction.new(
+        account_cvu: account.cvu,
+        amount: 50,
+        date: Date.today,
+        transaction_type: 'withdrawal'
+      )
+      expect(transaction).to be_valid
+    end
+  end
+
+  context 'after create callback' do
+    it 'debita el balance correctamente' do
+      Transaction.create!(
+        account_cvu: account.cvu,
+        amount: 40,
+        date: Date.today,
+        transaction_type: 'withdrawal'
+      )
+      expect(account.reload.balance).to eq(60)
+    end
+  end
+end
