2a1ab1ad7bf9c45c52aee848011c5bc7d701326b
Amade0o <amadeoemai@gmail.com>
2025-06-13 14:37:40 -0300

    El dni ahora puede tener 7 caracteres tambien

--- mi_app_sinatra/models/user.rb
+++ mi_app_sinatra/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true #valida que el nombre este presente
   validates :last_name, presence: true #valida que el apellido este presente
   validates :phone, presence: true, format: { with: /\A\d{7,15}\z/, message: "El telefono debe ser valido\n" } #valida que el telefono este presente
-  validates :dni, presence: true, uniqueness: true, format: {with: /\A\d{8}\z/,message: "debe tener exactamente 8 dígitos numéricos, sin puntos ni letras\n"} #valida que el dni este presente y sea unico
+  validates :dni, presence: true, uniqueness: true, format: {with: /\A\d{7,8}\z/,message: "debe tener exactamente 8 dígitos numéricos, sin puntos ni letras\n"} #valida que el dni este presente y sea unico
   validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP,message: "No es un email valido\n"} #valida que el email este presente y sea unico
 end
 
