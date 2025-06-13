4a0321dc6f0bdda56c77beb09e2661f509243f22
AgustinCoup <tutyboss12@gmail.com>
2025-06-13 15:35:41 -0300

    Pantallas traducidas y agrego validación de longitud para contraseña

--- mi_app_sinatra/models/user.rb
+++ mi_app_sinatra/models/user.rb
@@ -6,9 +6,10 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true #valida que el nombre este presente
   validates :last_name, presence: true #valida que el apellido este presente
-  validates :phone, presence: true, format: { with: /\A\d{7,15}\z/, message: "El telefono debe ser valido\n" } #valida que el telefono este presente
-  validates :dni, presence: true, uniqueness: true, format: {with: /\A\d{7,8}\z/,message: "debe tener exactamente 8 dígitos numéricos, sin puntos ni letras\n"} #valida que el dni este presente y sea unico
-  validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP,message: "No es un email valido\n"} #valida que el email este presente y sea unico
+  validates :phone, presence: true, format: { with: /\A\d{7,15}\z/, message: "must be valid\n" } #valida que el telefono este presente
+  validates :dni, presence: true, uniqueness: true, format: {with: /\A\d{7,8}\z/,message: "must be 7 or 8 numerical digits\n"} #valida que el dni este presente y sea unico
+  validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP,message: "must be valid\n"} #valida que el email este presente y sea unico
+  validates :password, presence: true, length: { minimum: 6, message: "must have at least 6 characters\n" } #valida que la contraseña este presente y tenga al menos 6 caracteres
 end
 
 
